d4acd812284306926a6aaf77bc34da7f6882184f
Victor Navarro <vm.navarro94@gmail.com>
2020-06-06 23:48:57 -0500

    implemented api call, initial state and service layer (#2)
    checked

--- src/Constants/api_url.js
+++ src/Constants/api_url.js
@@ -1,1 +1,2 @@
-const api_key = '6c1e8757b9d05f8f0068f6b08ddf3d11';
+export const api_key = '6c1e8757b9d05f8f0068f6b08ddf3d11';
+export const url_base_weather = 'http://api.openweathermap.org/data/2.5/weather';
